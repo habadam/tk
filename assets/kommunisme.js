1c9184dfbc93042c730fe51c7795e8fc4fac6552
siravelan <arinveland@gmail.com>
2017-01-12 09:45:55 +0100

    Create kommunisme.js

---
+++ assets/kommunisme.js
@@ -0,0 +1,24 @@
+var kommunisme = function () {
+
+var title = Math.random();
+
+var surname = Math.random();
+
+var userName = prompt ("Hva er ditt fornavn?");
+
+if (title <= 0.2) {
+title = "comrade"
+}
+else if (title > 0.2 && title <= 0.4) {
+title = "Chairman"
+}
+else if (title > 0.4 && title <= 0.6) {
+title = "Eternal President"
+}
+else if (title > 0.6 && title <= 0.8) {
+title = "Shining Red Sun"
+}
+else if (title > 0.8) {
+title = "Revolutionary"
+}
+};
