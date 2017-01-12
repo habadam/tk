5f3b4145a8a4089dcbc342a1cd5f0902aa77c9d1
siravelan <arinveland@gmail.com>
2017-01-12 10:00:40 +0100

    Update kommunisme.js

--- assets/kommunisme.js
+++ assets/kommunisme.js
@@ -7,18 +7,37 @@ var surname = Math.random();
 var userName = prompt ("Hva er ditt fornavn?");
 
 if (title <= 0.2) {
-title = "comrade"
+title = "Comrade";
 }
 else if (title > 0.2 && title <= 0.4) {
-title = "Chairman"
+title = "Chairman";
 }
 else if (title > 0.4 && title <= 0.6) {
-title = "Eternal President"
+title = "Eternal President";
 }
 else if (title > 0.6 && title <= 0.8) {
-title = "Shining Red Sun"
+title = "Shining Red Sun";
 }
 else if (title > 0.8) {
-title = "Revolutionary"
+title = "Revolutionary";
 }
+
+if (surname <= 0.2) {
+surname = "Stalin";
+}
+else if (surname > 0.2 && surname <= 0.4) {
+surname = "Mao";
+}
+else if (surname > 0.4 && surname <= 0.6) {
+surname = "Castro";
+}
+else if (surname > 0.6 && surname <= 0.8) {
+surname = "Marx";
+}
+else if (surname > 0.8) {
+surname = "Kim";
+}  
+  
+alert ("Your communist name is "+title+" "+userName+" "+surname+"!");
+  
 };
