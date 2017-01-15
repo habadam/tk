1d9f5b073c5ca16334185f88857646f1fe65781a
tjespe <tord0505@ELVNB1775.Osloskole.no>
2017-01-15 01:36:11 +0100

    Forkortet skriptet en hel del

--- assets/kommunisme.js
+++ assets/kommunisme.js
@@ -1,42 +1,12 @@
 var kommunisme = function () {
 
-var title = Math.random();
-
-var surname = Math.random();
-
 var userName = prompt ("What is your first name?");
 
-if (title <= 0.2) {
-title = "Comrade";
-}
-else if (title > 0.2 && title <= 0.4) {
-title = "Chairman";
-}
-else if (title > 0.4 && title <= 0.6) {
-title = "Eternal President";
-}
-else if (title > 0.6 && title <= 0.8) {
-title = "Shining Red Sun";
-}
-else if (title > 0.8) {
-title = "Revolutionary";
-}
+var titles = ["Comrade", "Chairman", "Eternal President", "Shining Red Sun", "Revolutionary"];
+title = titles[Math.floor(Math.random()*5)];
 
-if (surname <= 0.2) {
-surname = "Stalin";
-}
-else if (surname > 0.2 && surname <= 0.4) {
-surname = "Mao";
-}
-else if (surname > 0.4 && surname <= 0.6) {
-surname = "Castro";
-}
-else if (surname > 0.6 && surname <= 0.8) {
-surname = "Marx";
-}
-else if (surname > 0.8) {
-surname = "Kim";
-}  
+var surnames = ["Stalin", "Mao", "Castro", "Marx", "Kim"];
+surname = surnames[Math.floor(Math.random()*5)];
   
 alert ("Your communist name is "+title+" "+userName+" "+surname+"!");
   
