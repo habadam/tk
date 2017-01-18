7510f4fa9b362d4c62f06a1267e26247e936a9c4
tjespe <tordjohanespe@gmail.com>
2017-01-18 18:09:53 +0100

    Formatted code

--- assets/hillary.js
+++ assets/hillary.js
@@ -1,53 +1,46 @@
-var hillary = function () {
-  if (confirm ("Er du klar for en skrekkelig opplevelse i eget hjem?")) {
-    alert ("Tenkte meg det, ja");
-    var age=prompt ("Før vi begynner må jeg bare vite en ting, hvor gammel er du?").toLowerCase();
-    if (age<16) {
-      alert ("Hvorfor leker du at du skjønner politikk, lille unge?");
-    } else {
-      alert ("Flott, du er gammel nok!");
-    }
+var hillary = function() {
+	if(confirm("Er du klar for en skrekkelig opplevelse i eget hjem?")) {
+		alert("Tenkte meg det, ja");
+		var age=prompt("Før vi begynner må jeg bare vite en ting, hvor gammel er du?").toLowerCase();
+		if (age<16) {
+			alert("Hvorfor leker du at du skjønner politikk, lille unge?");
+		} else {
+			alert("Flott, du er gammel nok!");
+		}
 
-    alert ("Du våkner om natta av noen merkelige lyder. Da du går til stua for å sjekke hva det er, ser du til din store skrekk at det er Hillary Clinton som er i ferd med å spise barnet ditt!");
-    var userAnswer = prompt("Hva gjør du nå? (Svar 'Sloss', 'Stikke', 'Bestikke', eller noe annet)").toLowerCase();
+		alert("Du våkner om natta av noen merkelige lyder. Da du går til stua for å sjekke hva det er, ser du til din store skrekk at det er Hillary Clinton som er i ferd med å spise barnet ditt!");
+		var userAnswer = prompt("Hva gjør du nå? (Svar 'Sloss', 'Stikke', 'Bestikke', eller noe annet)").toLowerCase();
+		if (userAnswer === "sloss") {
+			var userAnswer2 = prompt("Har du automatvåpen i huset? (Svar 'Ja', eller 'Nei')");
+			if (userAnswer2 === "ja") {
+				alert("Du benytter deg av din rett fra andre grunnlovstillegg, og skyter demokraten som er på eiendommen din uten tillatelse. Godt gjordt!");
+			} else {
+				alert("Du angriper Hillary med nevene, men det hjelper lite. Både du og barnet ditt blir spist. Nå angrer du kanskje på at du sa, 'nobody needs an assault rifle'");
+			}
+		}	else if (userAnswer === "stikke") {
+			var userAnswer2 = prompt("Er du sikker på at du vil overlate barnet ditt til en sikker død? (Svar: 'Ja', eller 'Nei')".toLowerCase());
+			if (userAnswer2 === 'ja') {
+				confirm ("Barnet ditt er dødt og Hillary er president, og alt er din skyld! Fornøyd med deg selv?")
+			}	else {
+				userAnswer
+			}
+		}	else if (userAnswer === "bestikke") {
+			alert("Med en gang du nevner bestikkelser ser Hillary interessert opp.");
+			var userAnswer2 = prompt("Er du såkalt 'really rich'? (Svar 'Ja', eller 'Nei'").toLowerCase();
+			if (userAnswer2 === "ja") {
+				alert("Hillary lar deg og barnet ditt gå. Dere kom unna denne gangen, men Hillary er fortsatt der ute.")
+			} else {
+				alert("Tips til deg: ikke prøv å bruke penger du ikke har, det funker bare for politikere. Du og ditt barn ender som døde.");
+			}
+		}	else {
+			alert("Du blir stående apatisk og se på at barnet blir fortært og jobben din blir flyttet til Kina (landet som fant opp global oppvarming) og det kommer 650 millioner innvandrere i uka.");
+		}
 
-    if (userAnswer === "sloss") {
-      var userAnswer2 = prompt ("Har du automatvåpen i huset? (Svar 'Ja', eller 'Nei')");
-      if (userAnswer2 === "ja") {
-        alert ("Du benytter deg av din rett fra andre grunnlovstillegg, og skyter demokraten som er på eiendommen din uten tillatelse. Godt gjordt!");
-      }
-      else {
-        alert ("Du angriper Hillary med nevene, men det hjelper lite. Både du og barnet ditt blir spist. Nå angrer du kanskje på at du sa, 'nobody needs an assault rifle'");
-      }
-    }
-    else if (userAnswer === "stikke") {
-      var userAnswer2 = prompt ("Er du sikker på at du vil overlate barnet ditt til en sikker død? (Svar: 'Ja', eller 'Nei')".toLowerCase());
-      if (userAnswer2 === 'ja') {
-        confirm ("Barnet ditt er dødt og Hillary er president, og alt er din skyld! Fornøyd med deg selv?")
-      }
-      else {
-        userAnswer
-      }
-    }
-    else if (userAnswer === "bestikke") {
-      alert ("Med en gang du nevner bestikkelser ser Hillary interessert opp.");
-      var userAnswer2 = prompt ("Er du såkalt 'really rich'? (Svar 'Ja', eller 'Nei'").toLowerCase();
-      if (userAnswer2 === "ja") {
-        alert ("Hillary lar deg og barnet ditt gå. Dere kom unna denne gangen, men Hillary er fortsatt der ute.")
-      } else {
-        alert ("Tips til deg: ikke prøv å bruke penger du ikke har, det funker bare for politikere. Du og ditt barn ender som døde.");
-      }
-    }
-    else {
-      alert ("Du blir stående apatisk og se på at barnet blir fortært og jobben din blir flyttet til Kina (landet som fant opp global oppvarming) og det kommer 650 millioner innvandrere i uka.");
-    }
-
-    var userRating = prompt ("Hvor bra synes du spillet var, på en skala fra 1-10. (Husk at Trump bomber dritten ut av deg og familien og tar oljen din, hvis du ikke gir bra rating");
-    if (userRating < 9) {
-      alert ("Du er nok terrorist, du! Du kommer til å angre på den ratingen når Trump kommer og såkalt 'grabs you by the pussy'!");
-    }
-    else {
-      alert ("Jeg og The Donald er glade for at du likte spillet. Trump lurer også på om du vil være med ned på grensa for å bygge mur. Mexico betaler deg, vi lover!");
-    }
-  }
+		var userRating = prompt("Hvor bra synes du spillet var, på en skala fra 1-10. (Husk at Trump bomber dritten ut av deg og familien og tar oljen din, hvis du ikke gir bra rating");
+		if (userRating < 9) {
+			alert("Du er nok terrorist, du! Du kommer til å angre på den ratingen når Trump kommer og såkalt 'grabs you by the pussy'!");
+		}	else {
+			alert("Jeg og The Donald er glade for at du likte spillet. Trump lurer også på om du vil være med ned på grensa for å bygge mur. Mexico betaler deg, vi lover!");
+		}
+	}
 };
