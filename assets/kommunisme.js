a6c4527e3dfe2a367c0961abcaa89ceec31354e8
tjespe <tordjohanespe@gmail.com>
2017-01-18 17:51:11 +0100

    Shortened code

--- assets/kommunisme.js
+++ assets/kommunisme.js
@@ -1,13 +1,4 @@
 var kommunisme = function () {
-
-var userName = prompt ("What is your first name?");
-
-var titles = ["Comrade", "Chairman", "Eternal President", "Shining Red Sun", "Revolutionary"];
-title = titles[Math.floor(Math.random()*5)];
-
-var surnames = ["Stalin", "Mao", "Castro", "Marx", "Kim"];
-surname = surnames[Math.floor(Math.random()*5)];
-  
-alert ("Your communist name is "+title+" "+userName+" "+surname+"!");
-  
+	var titles = ["Comrade", "Chairman", "Eternal President", "Shining Red Sun", "Revolutionary"], surnames = ["Stalin", "Mao", "Castro", "Marx", "Kim"];
+	alert("Your communist name is "+titles[Math.floor(Math.random()*5)]+" "+prompt("What is your first name?")+" "+surnames[Math.floor(Math.random()*5)]+"!");
 };
