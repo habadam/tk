eb3572a8f149db46fd8a0ba418d52129230c7875
tjespe <tordjohanespe@gmail.com>
2017-02-14 11:08:13 +0100

    Deleted directory

--- files/kommunismen/kommunisme.js
+++
@@ -1,4 +0,0 @@
-var kommunisme = function () {
-	var titles = ["Comrade", "Chairman", "Eternal President", "Shining Red Sun", "Revolutionary"], surnames = ["Stalin", "Mao", "Castro", "Marx", "Kim"];
-	alert("Your communist name is "+titles[Math.floor(Math.random()*5)]+" "+prompt("What is your first name?")+" "+surnames[Math.floor(Math.random()*5)]+"!");
-};
